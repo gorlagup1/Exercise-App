8eada292fe91c2c94c38bfd03753a9b84292f792
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-08 16:52:34 -0500

    db.js updated

---
+++ Server1/config/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+let pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  connectionLimit: process.env.DB_POOL || 10,
+  database: process.env.DB_NAME || "fitness_tracker_2",
+  port:3306,
+});
+
+pool.getConnection(function (err, connection) {
+  if (err) console.log('Error while connecting to databse', err);
+  else {
+    console.log('Database connected');
+    global.connection = connection;
+  }
+});
