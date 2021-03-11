9c3f301dc9ffd958ef290574c8ec8b4124985c6d
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-11 14:38:25 -0500

    --- Auto-Git Commit ---

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3000;
+var PORT = Process.env.PORT || 5000;
 applicationCache.addEventListener(port, function () {
     console.log("Server started successfully");
 });
