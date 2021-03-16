2c67265a5ffe428a2dd9dd76557c62e8763f3316
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-16 10:40:17 -0400

    --- Auto-Git Commit ---

--- Server1/config/db.js
+++ Server1/config/db.js
@@ -9,6 +9,15 @@ let pool = mysql.createPool({
   port:3306,
 });
 
+let pool = mysql.createPool({
+  host: process.env.DB_HOST || "sql5.freesqldatabase.com",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  connectionLimit: process.env.DB_POOL || 10,
+  database: process.env.DB_NAME || "fitness_tracker_2",
+  port:3306,
+});
+
 pool.getConnection(function (err, connection) {
   if (err) console.log('Error while connecting to databse', err);
   else {
