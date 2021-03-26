d90c22708a4c0c3b04ee60984ca0317524f4e4b8
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-25 22:33:03 -0400

    made minor changes to connections frinds.js

--- Server1/controllers/friend.js
+++ Server1/controllers/friend.js
@@ -1,14 +1,13 @@
 exports.getAllFriends = function (req, res) {
-    var sql = `Select * from users where id!=${req.query.id}`;
-    connection.query(sql, function (err, result) {
-      if (err) {
-        console.log(err);
-        res.json(err)
-      } else {
-        res.json({
-          friends: result
-        });
-      }
-    });
-  }
-  
+  var sql = `SELECT * from users WHERE email LIKE '%${req.query.searchText}%' OR name LIKE '%${req.query.searchText}%'`;
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.json(err)
+    } else {
+      res.json({
+        friends: result
+      });
+    }
+  });
+}
