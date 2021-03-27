1312bd4a212a8006c1127e0725101911c99b0378
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-03-26 20:09:46 -0400

    updated app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,27 +5,7 @@
 </template>
 
 <script>
-  /*--global FB
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      cookie: true,  // enable cookies to allow the server to access the session
-      xfbml: true,  // parse social plugins on this page
-      appId: '620057538167941',
-      autoLogAppEvents: true,
-      version: 'v2.6'
-    });
-  };
-*/
+  
   export default {
     name: 'app',
   };
