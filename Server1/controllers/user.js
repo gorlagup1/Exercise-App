c24571719c6cde4bd8d9610b192610ca34a00045
pavani <gorlagup1@hawkmail.newpaltz.edu>
2021-04-05 19:06:45 -0400

    changes made  to cryptr

--- Server1/controllers/user.js
+++ Server1/controllers/user.js
@@ -3,8 +3,8 @@
  */
 
  const jwt = require('jsonwebtoken');
- const atob = require('atob');
- const Cryptr = require('cryptr'),
+ const atob = require('btoa');
+ const cryptr = require('cryptr'),
  cryptr = new Cryptr(config.CRYPTOKEY);
  
  exports.register = (req, res) => {
