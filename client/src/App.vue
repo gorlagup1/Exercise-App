45ac21b669160996dad0c265df2570871bfe1c3a
gorlagup1 <gorlagup1@hawkmail.newpaltz.edu>
2021-04-28 23:43:54 -0400

    changed

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,9 +2,6 @@
   <div id="app">
     <router-view/>
   </div>
-
- 
-
 </template>
 
 <script>
@@ -12,28 +9,6 @@
   export default {
     name: 'app',
   };
-
-  /*global fb*/
-         window.fbAsyncInit = function() {
-          FB.init({
-            appId      : '509660520411411',
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v10.0'
-          });
-      
-          FB.AppEvents.logPageView();   
-      
-        }; 
-
-        (function(d, s, id){
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_US/sdk.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-
 
   // FIXME Please move the css to a seperate CSS File.
   // It seems the css written below is not plain css, but LESS or PostCSS
